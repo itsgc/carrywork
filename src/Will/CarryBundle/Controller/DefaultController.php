67ae8b00339bc862f12164098bdc300e3e92c1e7
itsgc <gc@didgt.info>
2012-06-12 22:58:01 +0200

    Primo tentativo form

--- src/Will/CarryBundle/Controller/DefaultController.php
+++ src/Will/CarryBundle/Controller/DefaultController.php
@@ -13,3 +13,54 @@ class DefaultController extends Controller
         return $this->render('WillCarryBundle:Default:index.html.twig', array('name' => $name));
     }
 }
+// src/Will/CarryBundle/Controller/DefaultController.php
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Will\CarryBundle\Entity\Task;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+	public function newAction(Request $request)
+	{
+		// create a task and give it some dummy data for this example
+		$task = new Task();
+		$task->setTask('Write a blog post');
+		$task->setDueDate(new \DateTime('tomorrow'));
+
+		$form = $this->createFormBuilder($task)
+		->add('task', 'text')
+		->add('dueDate', 'date')
+		->getForm();
+
+		return $this->render('WillCarryBundle:Default:new.html.twig', array(
+				'form' => $form->createView(),
+		));
+	}
+}
+
+// ...
+
+public function newAction(Request $request)
+{
+	// just setup a fresh $task object (remove the dummy data)
+	$task = new Task();
+
+	$form = $this->createFormBuilder($task)
+	->add('task', 'text')
+	->add('dueDate', 'date')
+	->getForm();
+
+	if ($request->getMethod() == 'POST') {
+		$form->bindRequest($request);
+
+		if ($form->isValid()) {
+			// perform some action, such as saving the task to the database
+
+			return $this->redirect($this->generateUrl('task_success'));
+		}
+	}
+
+	// ...
+}
---
+++ src/Will/CarryBundle/Entity/Task.php
@@ -0,0 +1,27 @@
+// src/Will/CarryBundle/Entity/Task.php
+namespace Will\CarryBundle\Entity;
+
+class Task
+{
+    protected $task;
+
+    protected $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
+}
