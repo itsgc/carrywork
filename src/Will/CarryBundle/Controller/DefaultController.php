e861f3de549d6fa310bbcc2b182abfa4ac111135
itsgc <gc@didgt.info>
2012-06-12 23:22:05 +0200

    DefCont

--- src/Will/CarryBundle/Controller/DefaultController.php
+++ src/Will/CarryBundle/Controller/DefaultController.php
@@ -3,7 +3,7 @@
 namespace Will\CarryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use src\Will\CarryBundle\Entity\Task.php;
+use src\Will\CarryBundle\Entity\Task;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
